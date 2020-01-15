ec17cb6592f4514b796c9dad81815ee0339b55ee
Jacob York <Jacob.C.York-1@ou.edu>
2020-01-15 13:17:35 -0600

    Created project and solution

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Jacob");
+            Console.Write("York");
+
+            Console.Beep(2000, 500);
+
+
+            Console.ReadKey();
+        }
+    }
+}
